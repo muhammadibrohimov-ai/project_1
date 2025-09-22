4a14977f7886b840f35e9841cc502521e8892da1
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-09-22 20:36:52 +0500

    added models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+# User model
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    year = models.PositiveIntegerField()
+    address = models.CharField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+# Product model
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(null=True)
+    price = models.DecimalField(max_length=10, max_digits=8, decimal_places=3)
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+    
+    def __str__(self):
+        return self.name
+    
+# Order model
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.id
+    
